dba613997d89108d09737d23d0595f701c7dc437
Manender <Manendeer@gmail.com>
2026-05-23 22:32:58 +0500

    Smooooooth camera

--- orbit_camera.gd
+++ orbit_camera.gd
@@ -3,11 +3,27 @@ extends Node3D
 @onready var shipment_menu: = %ShipmentMenu
 
 @export var sensivity: float = 0.01
+@export var camera: Camera3D
+
+@export_group("Zoom", "zoom_")
+@export_range(0.1, 100.0, 1.0, "or_greater", "exp", "suffix:m") var zoom_min_camera_distance: float = 10
+@export_range(10.0, 1000.0, 1.0, "or_less", "exp", "suffix:m") var zoom_max_camera_distance: float = 100
+@export_range(0.01, 1, 0.1, "or_greater", "or_less", "suffix:s") var zoom_animation_duration: float = 0.1
+
+var zoom_tween: Tween
 
 var rotation_x: float = 0.0
 var rotation_y: float = 0.0
 
 const HALF_PI = PI / 2
+
+func zoom(multiplier: float):
+	if zoom_tween: zoom_tween.kill()
+	zoom_tween = create_tween()
+	zoom_tween.tween_property(self, ^"camera:position:z", camera.position.z * multiplier, zoom_animation_duration)
+
+func _ready():
+	assert(is_instance_valid(camera), "Camera is not set")
 
 func _input(event: InputEvent) -> void:
 	if(shipment_menu.in_shipment_menu == false):
@@ -18,10 +34,12 @@ func _input(event: InputEvent) -> void:
 
 			basis = Basis.IDENTITY
 			rotate_object_local(Vector3.UP, -rotation_x)
-			rotate_object_local(Vector3.LEFT, rotation_y)
-
-		if event is InputEventMouseButton:
+			rotate_object_local(Vector3.LEFT, -rotation_y)
+		
+		elif event is InputEventMouseButton:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		if event is InputEventKey:
+			if event.is_action_pressed("camera_zoom_out"): zoom(2)
+			elif event.is_action_pressed("camera_zoom_in"): zoom(0.5)
+		elif event is InputEventKey:
 			if event.is_action_pressed("ui_cancel"):
 				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
