c8e14f93d2dfce2f33fd936ce505e1b5b236460e
Manender <Manendeer@gmail.com>
2026-05-24 19:49:48 +0500

    Improve airship controls, useless RotatingContainer stuff, potentially remntants of a really poort conflict resolution

--- airship/airship.gd
+++ airship/airship.gd
@@ -8,18 +8,18 @@ const g: float = 9.8
 
 var mission_number: int = -1
 
-@export_group("Cargo")
 @export var products: Array[Product] = []
 var crate_size: float = 100:
 	set(value):
-			crate_size = max(0, value)
+		crate_size = max(0, value)
 
 @export_group("Ballast")
-@export var airship_capasity: float = 50000.0
+@export var envelope: Node3D
+@export var airship_capacity: float = 50000.0
 @export var air_in_ballast: float:
 	set(value):
-		air_in_ballast = clamp(value, 0, airship_capasity)
-		air_changed_fraction.emit(air_in_ballast / airship_capasity)
+		air_in_ballast = clamp(value, 0, airship_capacity)
+		air_changed_fraction.emit(air_in_ballast / airship_capacity)
 
 @export var air_ballast_pump_speed :float = 1000.0
 
@@ -28,8 +28,6 @@ signal air_changed_fraction(new_fraction: float)
 @export_group("Engine")
 @export var acceleration: float = 15000.0
 @export var torque: float = 37500.0
-@export var acceleration_friction: float = 1.0
-@export var torque_friction: float = 1.0
 
 @export_group("Controls")
 ## Whether the player can control the airship.
@@ -50,8 +48,7 @@ signal on_controls_disable
 signal on_controls_change(new_state: bool)
 
 func _ready() -> void:
-	linear_damp = acceleration_friction
-	angular_damp = torque_friction
+	assert(envelope, "envelope is not set")
 
 	mass = weight
 	for prod in products:
@@ -62,21 +59,27 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if controls_enabled:
-		if Input.is_action_pressed("takeoff"):
+		if Input.is_action_pressed("airship_ascend"):
 			air_in_ballast -= air_ballast_pump_speed * delta
-		if Input.is_action_pressed("landing"):
-			air_in_ballast += air_ballast_pump_speed * delta
+		if Input.is_action_pressed("airship_descend"):
+			air_in_ballast += air_ballast_pump_speed * delta * Input.get_axis("airship_descend", "airship_ascend")
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	if controls_enabled:
-		var thrust_axis = Input.get_axis("airship_back", "airship_forward")
+		var thrust_axis: = Input.get_axis("airship_back", "airship_forward")
 		state.apply_central_force(-basis.z * thrust_axis * acceleration)
 
-		var torque_axis = Input.get_axis("airship_left", "airship_right")
-		state.apply_torque(Vector3(0, torque_axis * torque, 0))
+		var yaw_axis: = Input.get_axis("airship_yaw_right", "airship_yaw_left")
+		state.apply_torque(basis.y * yaw_axis * torque)
+
+		var pitch_axis: = Input.get_axis("airship_pitch_down", "airship_pitch_up")
+		state.apply_torque(basis.x * pitch_axis * torque)
+
+		var roll_axis: = Input.get_axis("airship_roll_right", "airship_roll_left")
+		state.apply_torque(basis.z * roll_axis * torque)
 
 	#airship takeoff and landing
-	state.apply_central_force(Vector3.UP * g * air_in_ballast * (air_density - airship_density))
+	state.apply_force(Vector3.UP * g * air_in_ballast * (air_density - airship_density), envelope.global_position - global_position)
 
 func add_mass():
 	for prod in products:
